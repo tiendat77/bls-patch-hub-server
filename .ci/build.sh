7608fb1d81dd354276aeb2068636f8d1600d5f28
Tien Dat Huynh <huynhztienzdat@gmail.com>
2024-06-26 09:58:40 +0700

    chore: 🐳 update ci scripts

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-GITHUB_USERNAME=$1
-GITHUB_PASSWORD=$2
-CONTAINER_RELEASE_IMAGE="ghcr.io/${GITHUB_USERNAME}/patch-hub-server:latest"
+source ~/.credentials
+
+GITHUB_USERNAME=$GITHUB_USERNAME
+GITHUB_PASSWORD=$GITHUB_PASSWORD
+
+CONTAINER_RELEASE_IMAGE="ghcr.io/${GITHUB_USERNAME}/patch-hub-server"
+
+VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
 
 # Print each command and exit on error
 set -xe
@@ -13,7 +23,15 @@ echo $GITHUB_PASSWORD | docker login ghcr.io -u $GITHUB_USERNAME --password-stdi
 
 # Build and push image
 echo '🐳  Building docker image with tag latest'
-docker build --platform "linux/amd64" -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
+docker build \
+  --platform "linux/amd64" \
+  -t ${CONTAINER_RELEASE_IMAGE}:latest \
+  -t ${CONTAINER_RELEASE_IMAGE}:${VERSION} \
+  -f Dockerfile . || exit 1
 
 echo '🚀  Pushing docker image to github registry'
-docker push ${CONTAINER_RELEASE_IMAGE}
+docker push ${CONTAINER_RELEASE_IMAGE}:latest || exit 1
+docker push ${CONTAINER_RELEASE_IMAGE}:${VERSION} || exit 1
+
+# Manually run command
+# docker build -t "ghcr.io/blogic-datht/patch-hub-server:latest" -f Dockerfile .
--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
-echo "🚀  Deploying via remote SSH"
 
 SERVER_IP='192.168.1.194'
-CONTAINER_NAME='patch-hub-server'
 
-ssh -i "/var/jenkins_home/.ssh/id_rsa" "bls@${SERVER_IP}" \
-  "docker pull ghcr.io/blogic-datht/${CONTAINER_NAME} \
-  && docker compose -f composes/${CONTAINER_NAME}/docker-compose.yaml down \
-  && docker compose -f composes/${CONTAINER_NAME}/docker-compose.yaml up -d \
-  && docker system prune -f"
+echo
+echo "                 %%%%%                            "
+echo "              %%%%%%%%  *                         "
+echo "             %%%%%%%%%  *****                     "
+echo "             %%%% %%%%  ********                  "
+echo "         %%  %%%  %%%%  ******+**                 "
+echo "       %%%%  %%%  %%%%  ***  +***                 "
+echo "       %%%%  %%%  %%%%  ***  ****                 "
+echo "       %%%%  %%%  %%%%  *+*  ****                 "
+echo "       %%%%  %%%  %%%%  ***  +***                 "
+echo "       %%%%  %%%  %%%%  ***  ****                 "
+echo "       %%%%  %%%  %%%%  ***  ****                 "
+echo "       %%%%%%%%%  %%%%  ***                       "
+echo "        %%%%%%%%  %%%%  ***                       "
+echo "           %%%%%  %%%%  +**                       "
+echo "              %%  %%%%  **                        "
+echo "                  %%%%                            "
+echo
+echo "🚀 Deploying via remote SSH ${SERVER_IP}          "
 
+
+ssh "bls@${SERVER_IP}" \
+  "docker compose -f composes/patch-hub/docker-compose.yaml pull \
+  && docker compose -f composes/patch-hub/docker-compose.yaml down \
+  && docker compose -f composes/patch-hub/docker-compose.yaml up -d \
+  && docker system prune -f" || exit 1
+
+echo
 echo "🎉 Successfully deployed, hooray!"
